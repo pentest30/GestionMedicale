50c861159fc167bd469399a087c66ca8b03f2e2c
pentest30 <pe>
2015-04-29 09:47:46 +0100

    fg

---
+++ GM.Services/Magasins/IServiceMagasin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using GM.Core.Models;
+
+namespace GM.Services.Magasins
+{
+    public interface IServiceMagasin
+    {
+        IEnumerable<Magasin> Liste();
+        //IEnumerable<Pays> ListePays();
+        bool Insert(Magasin laboratoire);
+        bool Update(Magasin laboratoire);
+        Magasin FindSingle(int id);
+        bool Delete(int id);
+    }
+}
--- GM.Services/Medicaments/IServiceMedicmaent.cs
+++ GM.Services/Medicaments/IServiceMedicmaent.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 using GM.Core.Models;
 
 namespace GM.Services.Medicaments
